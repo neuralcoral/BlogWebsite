b03ec95b5bc7212c302431cac49d00617eee0035
neuralcoral <george@neuralcoral.io>
2024-09-01 17:03:30 -0700

    Updated DraftPost

--- src/pages/DraftPost/DraftPost.tsx
+++ src/pages/DraftPost/DraftPost.tsx
@@ -4,35 +4,35 @@ import PostEditor from './PostEditor';
 import PostPreview from './PostPreview';
 import { FaEdit } from "react-icons/fa";
 import { VscOpenPreview } from "react-icons/vsc";
+import { useNavigate } from 'react-router-dom';
 
 const DraftPost: React.FC = () => {
+    const navigate = useNavigate();
     const [value, setValue] = useState("# Mark Down World");
     const [isEditor, setIsEditor] = useState(true);
 
     return (
-      <div className='draft-container'>
-            <div className='draft'>
-                <div className='draft-title'>
-                    <input value={'Title'}/>
-                </div>
-                <div className='draft-text'>
-                    {isEditor ? <PostEditor text={value} setText={setValue} /> : <PostPreview text={value} /> }
-                </div>
-                <div className='draft-buttons'>
-                    <button>
-                        Save
-                    </button>
-                    <button>
-                        Review
-                    </button>
-                </div>
+        <div className='draft'>
+            <div className='draft-title'>
+                <input value={'Title'}/>
+            </div>
+            <div className='draft-text'>
+                {isEditor ? <PostEditor text={value} setText={setValue} /> : <PostPreview text={value} /> }
             </div>
             <div className='side-buttons'>
                 <div className='preview-edit-toggle' onClick={() => setIsEditor(!isEditor)}>
                     {isEditor ? <VscOpenPreview /> : <FaEdit /> }
                 </div>
             </div>
-      </div>
+            <div className='bottom-buttons'>
+                <button>
+                    Save
+                </button>
+                <button onClick={() => navigate('/posts/review/1')}>
+                    Review
+                </button>
+            </div>
+        </div>
     );
 
 };
