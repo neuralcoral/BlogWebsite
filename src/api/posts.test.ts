9f088be5ba9bd5b9c75d50459e6f3c3dff8f2265
neuralcoral <george@neuralcoral.io>
2024-09-02 17:40:08 -0700

    Setup pakcages needed for test

--- src/api/posts.test.ts
+++ src/api/posts.test.ts
@@ -1,13 +1,27 @@
 import { Status } from '../models/post';
 import dummyPosts from './dummy-posts';
-import { getPost, getPostedPosts } from './posts';
+import { getDraftedPosts, getPosts, getPostedPosts } from './posts';
 
 const mockGetPostsCallback = jest.fn(() => dummyPosts);
 
+describe('Posts client Tests', () => {
+  test('getPostedPosts returns list of post posts', async () => {
+    const dummyPostedPosts = dummyPosts.filter((post) => post.status === Status.Posted);
+    expect(await getPostedPosts({}))
+    .toEqual(dummyPostedPosts);
+  });
 
-test('get posts returns list of posts', async () => {
-  const dummyPostedPosts = dummyPosts.filter((post) => post.status === Status.Posted);
-  expect(await getPostedPosts({}))
-  .toEqual(dummyPostedPosts);
+  test('getDraftedPosts returns list of drafted posts', async () => {
+    const dummpyDraftedPosts = dummyPosts.filter((post) => post.status === Status.Draft);
+    expect(await getDraftedPosts({}))
+    .toEqual(dummpyDraftedPosts);
+  });
 
+  test('getPosts returns list of posts', async () => {
+    expect(await getPosts({})).toEqual(dummyPosts);
+  });
+
+  test('get')
 })
+
+
--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,24 +1,6 @@
 import Post, { Status } from '../models/post';
 import dummyPosts from './dummy-posts';
 
-export const getPosts = async ({ signal, status }: { signal?: AbortSignal, status?:Status }): Promise<Post[]> => {
-  return new Promise((resolve, reject) => {
-    
-    let posts = dummyPosts;
-    if (status){
-      posts = dummyPosts.filter((post) => post.status === status);
-    }
-    const delay = setTimeout(() => {
-      resolve(posts);
-    }, 1000);
-
-    signal?.addEventListener('abort', () => {
-      clearTimeout(delay);
-      reject(new DOMException('The user aborted a request.', 'AbortError'));
-    });
-  });
-}
-
 export const getPostedPosts = async ({ signal }: { signal?: AbortSignal }): Promise<Post[]> => {
   const status = Status.Posted;
   return getPosts({signal , status});
@@ -28,6 +10,24 @@ export const getDraftedPosts =  async ({ signal }: { signal?: AbortSignal }): Pr
   const status = Status.Draft;
   return getPosts({signal , status});
 };
+
+export const getPosts = async ({ signal, status }: { signal?: AbortSignal, status?:Status }): Promise<Post[]> => {
+  return new Promise((resolve, reject) => {
+    
+    let posts = dummyPosts;
+    if (status){
+      posts = dummyPosts.filter((post) => post.status === status);
+    }
+    const delay = setTimeout(() => {
+      resolve(posts);
+    }, 500);
+
+    signal?.addEventListener('abort', () => {
+      clearTimeout(delay);
+      reject(new DOMException('The user aborted a request.', 'AbortError'));
+    });
+  });
+}
 
 export const getPost = (id: string) => {
   const posts = dummyPosts.filter((post) => id === post.id);
