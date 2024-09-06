2c24dc1c8bd38f5f132acab74078057048ff744d
George 🦜 <104024088+neuralcoral@users.noreply.github.com>
2024-09-05 22:32:26 -0700

    Fully covered NavBar

--- src/components/NavBar/NavBar.test.tsx
+++ src/components/NavBar/NavBar.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import NavBar from "./NavBar";
 import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
 import { PAGES } from "./pages";
@@ -15,5 +15,51 @@ describe('NavBar component', () => {
     PAGES.map((page) => {
       expect(screen.getByText(page.pageName)).toBeInTheDocument();
     });
-  })  
+  });
+
+  test('toggle functionality works', () => {
+    render(
+      <Router>
+        <NavBar />
+      </Router>
+    );
+
+    PAGES.forEach((page) => {
+      expect(screen.getByText(page.pageName)).toBeVisible();
+    });
+
+    const toggleButton = screen.getByText('🧠');
+    fireEvent.click(toggleButton);
+
+    PAGES.forEach((page) => {
+      expect(screen.queryByText(page.pageName)).toBeNull();
+    });
+
+    fireEvent.click(toggleButton);
+
+    PAGES.forEach((page) => {
+      expect(screen.getByText(page.pageName)).toBeVisible();
+    });
+  });
+
+  test('clicking on a page navigates correctly', () => {
+    const mockNavigate = jest.fn();
+
+    jest.mock('react-router-dom', () => ({
+      useNavigate: () => mockNavigate,
+    }));
+
+    render(
+      <Router>
+        <NavBar />
+      </Router>
+    );
+
+    PAGES.forEach((page) => {
+      const pageItem = screen.getByText(page.pageName);
+      fireEvent.click(pageItem);
+      expect(mockNavigate).toHaveBeenCalledWith(page.route());
+    });
+  });
+
 })
