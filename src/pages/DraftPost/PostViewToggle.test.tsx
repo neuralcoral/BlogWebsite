267c4b4af6d707f62898d091a865f17df98c0d70
neuralcoral <george@neuralcoral.io>
2024-09-07 13:10:19 -0700

    Added tests for PostViewToggle

---
+++ src/pages/DraftPost/PostViewToggle.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import PostViewToggle from "./PostViewToggle";
+import { initialPost } from "../../test_utils/mock_data";
+
+jest.mock('./PostEditor', () => () => <div>Mock PostEditor</div>);
+jest.mock('./PostPreview', () => () => <div>Mock PostPreview</div>);
+
+describe('PostViewToggle tests', () => {
+  test('initial state', () => {
+    const mockSetPost = jest.fn();
+    render(
+      <PostViewToggle post={initialPost} setPost={mockSetPost} isEditing={true}/>
+    )
+    expect(screen.getByText(/Mock PostEditor/i)).toBeInTheDocument();
+  });
+
+  test('preview works', () => {
+    const mockSetPost = jest.fn();
+    render(
+      <PostViewToggle post={initialPost} setPost={mockSetPost} isEditing={false}/>
+    )
+    expect(screen.getByText(/Mock PostPreview/i)).toBeInTheDocument();
+  });
+
+});
